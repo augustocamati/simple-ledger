e1ebdc8621eed527fdd0140228ce26cae39e9bf8
Augusto Camati <augustocamati21@gmail.com>
2026-07-14 01:37:11 +0100

    feat: create  interface for entriess in transaction output and input class

--- src/application/use-cases/transaction/CreateTransactionInput.ts
+++ src/application/use-cases/transaction/CreateTransactionInput.ts
@@ -1,11 +1,13 @@
 import { Direction } from "../../../domain/shared/Direction"
 
+
+export interface CreateTransactionEntryInput {
+  accountId: string
+  direction: Direction
+  amount: number
+}
 export interface CreateTransactionInput {
   id?: string
   name: string
-  entries: {
-    accountId: string
-    direction: Direction
-    amount: number
-  }[]
+  entries: CreateTransactionEntryInput[]
 }
--- src/application/use-cases/transaction/CreateTransactionOutput.ts
+++ src/application/use-cases/transaction/CreateTransactionOutput.ts
@@ -1,11 +1,13 @@
 import { Direction } from "../../../domain/shared/Direction"
 
+export interface CreateTransactionEntryInput {
+    id?: string
+  accountId: string
+  direction: Direction
+  amount: number
+}
 export interface CreateTransactionOutput {
-    id: string
-    name: string
-    entries: {
-        accountId: string
-        direction: Direction
-        amount: number
-    }[]
+  id: string
+  name: string
+  entries: CreateTransactionEntryInput[]
 }

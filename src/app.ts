c1d120682d1f461b65cf29d477189620669079d5
Augusto Camati <augustocamati21@gmail.com>
2026-07-15 23:59:54 +0100

    feat: register routes

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,16 @@
-import fastify from "fastify";
-import cors from "@fastify/cors";
+import fastify from "fastify"
+import cors from "@fastify/cors"
+import { accountRoutes } from "./http/routes/accountRoutes"
+import { transactionRoutes } from "./http/routes/transactionRoutes"
 
 export const app = fastify()
 
 app.register(cors)
 
-app.get('/ping', () => {
+app.register(accountRoutes)
+
+app.register(transactionRoutes)
+
+app.get("/ping", () => {
   return { pong: true }
 })

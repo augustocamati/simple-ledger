a81a5f87cc11f07c66146291b7e2a6868b41e2c6
Augusto Camati <augustocamati21@gmail.com>
2026-07-15 23:40:36 +0100

    feat: create  transaction controller

--- src/http/controllers/TransactionController.ts
+++ src/http/controllers/TransactionController.ts
@@ -1,20 +1,8 @@
 import { makeCreateTransactionUseCase } from "../../main/factories/makeCreateTransactionUseCase"
 import { FastifyReply, FastifyRequest } from "fastify"
 import { z } from "zod"
+import { createTransactionSchema } from "../validator/TransactionValidator"
 
-export const createTransactionSchema = z.object({
-  name: z.string(),
-
-  entries: z.array(
-    z.object({
-      accountId: z.string(),
-
-      direction: z.enum(["debit", "credit"]),
-
-      amount: z.number(),
-    }),
-  ),
-})
 
 
 export class TransactionController {
